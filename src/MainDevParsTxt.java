59f4b0bac443d414c1481e5d61eb5d256b1f645b
mackret <kret86@yandex.ru>
2018-01-16 01:24:12 +0300

    night commit

--- src/MainDevParsTxt.java
+++ src/MainDevParsTxt.java
@@ -7,14 +7,18 @@ public class MainDevParsTxt {
 
 
     static String nameOfTXTFile =
-            //            "//Users//mackret//Downloads/dev.txt";
-            "C://Users//SBT-Kretov-AA//Downloads/12_dev.txt";
+                        "//Users//mackret//Downloads/12_dev.txt";
+            //"C://Users//SBT-Kretov-AA//Downloads/12_dev.txt";
+
+    //static String[]arrPrefFromSM = {"PM","IM","ЗНО","PT","RSK","ЗНР"};
+
     static String textFromTXTFile = "";
 
     public static void main (String[]xxx) {
 
         String regexBegin = "Цуп/Тск/Sm";
-        String regex1 = "CRM-";
+        String regexCRM = "CRM-";
+        String regexTab = "\t";
         String regex2 = "Проблем:";
         String regex3 = "Обновить";
         String regexEnd = "Нравится";
@@ -31,10 +35,10 @@ public class MainDevParsTxt {
             System.out.println(ex.getMessage());
         }
 
-        System.out.println(textFromTXTFile);
+        //System.out.println(textFromTXTFile);
 
 
-//разбиваем строку по символу начала новой строки
+//разбиваем строку в массив по символу начала новой строки
 String[]buffArrFromTXT = textFromTXTFile.split("\n");
 
 for (int i = 0; i < buffArrFromTXT.length; i ++) {
@@ -42,9 +46,33 @@ for (int i = 0; i < buffArrFromTXT.length; i ++) {
     if (buffArrFromTXT[i].contains(regexBegin)) {
         continue;
     } else {
-        System.out.println(buffArrFromTXT[i]);
+//обработка остатка строки от "CRM-"
+        if (buffArrFromTXT[i].contains(regexCRM)) {
+
+            int indexTab = buffArrFromTXT[i].indexOf(regexTab);
+
+                if (buffArrFromTXT[i].contains(regex2)) {
+                    int indexRegex2 = buffArrFromTXT[i].indexOf(regex2);
+                    int indexRegex3 = buffArrFromTXT[i].indexOf(regex3);
+                    buffArrFromTXT[i] = buffArrFromTXT[i].replaceAll((buffArrFromTXT[i].substring(indexRegex2, indexRegex3 + regex3.length())), "");
+                }
+
+            buffArrFromTXT[i] = buffArrFromTXT[i].substring(0, indexTab) + regexTab + "(" + buffArrFromTXT[i].substring(indexTab + regexTab.length()) + ")";
+
+        }
+
+        //System.out.println(buffArrFromTXT[i]);
     }
 }
+
+//теперь в новом цикле удаляем пустые строки (типа дефекты без заявок в SM)
+        for (String i: buffArrFromTXT) {
+            if (i.contains("()")) {
+               i = "";
+            }
+
+            System.out.println(i);
+        }
 
 
     } //end of method
