9ae51987542499902cf0c2cf2c58181a7f49a638
mackret <kret86@yandex.ru>
2018-01-18 02:03:37 +0300

    night commit

--- src/MainDevParsTxt.java
+++ src/MainDevParsTxt.java
@@ -10,9 +10,11 @@ public class MainDevParsTxt {
                         "//Users//mackret//Downloads/12_dev.txt";
             //"C://Users//SBT-Kretov-AA//Downloads/12_dev.txt";
 
-    //static String[]arrPrefFromSM = {"PM","IM","ЗНО","PT","RSK","ЗНР"};
-
     static String textFromTXTFile = "";
+    static int empCount = 0;
+    static int totalCount = 0;
+
+    static String stringBuff = "";
 
     public static void main (String[]xxx) {
 
@@ -21,7 +23,6 @@ public class MainDevParsTxt {
         String regexTab = "\t";
         String regex2 = "Проблем:";
         String regex3 = "Обновить";
-        String regexEnd = "Нравится";
 
 //из тексового файла все переводим в тектовую переменную строку
         try {
@@ -60,21 +61,70 @@ for (int i = 0; i < buffArrFromTXT.length; i ++) {
             buffArrFromTXT[i] = buffArrFromTXT[i].substring(0, indexTab) + regexTab + "(" + buffArrFromTXT[i].substring(indexTab + regexTab.length()) + ")";
 
         }
+//убираем пустые дефекты без привязки к запросам из СМ и корректируем некоторые неровности
+        if (buffArrFromTXT[i].contains("()") ) {
+            continue;
+        } else {
+           // System.out.println(buffArrFromTXT[i]);
+            buffArrFromTXT[i] = buffArrFromTXT[i].replace(" )",")");
+            buffArrFromTXT[i] = buffArrFromTXT[i].replace("\t("," (");
+            stringBuff+=buffArrFromTXT[i] + "\n";
+        }
 
-        //System.out.println(buffArrFromTXT[i]);
+//получаем количество разработчиков
+            if (buffArrFromTXT[i].contains(regexCRM)) {
+                continue;
+            } else if (buffArrFromTXT[i].length()<1) {
+                continue;
+            }    else {
+                    empCount++;
+            }
+
     }
 }
 
-//теперь в новом цикле удаляем пустые строки (типа дефекты без заявок в SM)
-        for (String i: buffArrFromTXT) {
-            if (i.contains("()")) {
-               i = "";
-            }
+        System.out.println(empCount);
+        //System.out.println(stringBuff);
 
-            System.out.println(i);
+//обратно строку в массив
+
+    String[]stringBuffArr = stringBuff.split("\n");
+
+
+
+//ищем дубли в массиве - типа одинаковые задачи
+    int count = 0;
+    int countDoubles = 0;
+
+    for (int i = 0; i < stringBuffArr.length; i++) {
+        if (stringBuffArr[i].length() != 0) {
+            for (int j = 0; j < stringBuffArr.length; j++) {
+                if (stringBuffArr[j].length() != 0) {
+
+
+                    if (j != i && stringBuffArr[j].equals(stringBuffArr[i])) {
+                        countDoubles++;
+                        System.out.println(countDoubles +" "+ stringBuffArr[i]);
+                        break;
+                    }
+
+
+                }
+            }
         }
 
+        //System.out.println(count + ". " + stringBuffArr[i]);
+    }
+
+    System.out.println("дублей найдено: " + countDoubles);
 
     } //end of method
 
 } //end of class
+
+
+/*
+CRM-79722	(PM00036576, IM55808744)
+Анохина, Жердева, Марков - должен остаться у Жердевой
+*/
+
